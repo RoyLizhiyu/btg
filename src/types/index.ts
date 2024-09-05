08a0b6225d1d3aa19134adc18a33e2453f34f51c
Zhiyu Li <royzhiyu.li@alumni.utoronto.ca>
2024-09-05 16:12:46 -0400

    add redux toolkit, axios. server api WIP

---
+++ src/types/index.ts
@@ -0,0 +1,29 @@
+type track = {
+  key:
+    | "c"
+    | "g"
+    | "d"
+    | "a"
+    | "e"
+    | "b"
+    | "gb"
+    | "db"
+    | "ab"
+    | "eb"
+    | "bb"
+    | "f"
+    | "am"
+    | "em"
+    | "bm"
+    | "f#m"
+    | "c#m"
+    | "g#m"
+    | "ebm"
+    | "bbm"
+    | "fm"
+    | "cm"
+    | "gm"
+    | "dm";
+  genre: "jazz" | "rock" | "rnb" | "neoSoul" | "funk" | "blues" | "Ballad";
+  bpm: "slow" | "medium" | "fast";
+};
